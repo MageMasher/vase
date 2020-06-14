b1a76df2445f35a487ff5b72bd1ecc4a32cbd79d
Joe Lane <me@joelane.net>
2020-06-14 15:28:05 -0500

    Use retry, don't deref plugins, lazily create connection

--- samples/petstore-ions/src/petstore/ion.clj
+++ samples/petstore-ions/src/petstore/ion.clj
@@ -2,22 +2,23 @@
   (:require
     [datomic.client.api :as client]
     [fern :as f]
-    [io.pedestal.interceptor :as i]))
+    [io.pedestal.interceptor :as i]
+    [petstore.retry :as retry]))
 
 (defrecord LazyCloudConnection [client-config db-name]
   i/IntoInterceptor
   (-interceptor [_]
-    (let [client (client/client client-config)
-          _      (client/create-database client {:db-name db-name})
-          ]
-      (i/map->Interceptor
-        {:enter
-         (fn [ctx]
-           (let [cxn (client/connect client {:db-name db-name})]
-             (update ctx :request assoc
-                     :client client
-                     :conn cxn
-                     :db (client/db cxn))))}))))
+    (i/map->Interceptor
+      {:enter
+       (fn [ctx]
+         (let
+           [client (retry/with-retry #(client/client client-config))
+            ;_      (client/create-database client {:db-name db-name})
+            cxn (retry/with-retry #(client/connect client {:db-name db-name}))]
+           (update ctx :request assoc
+                   :client client
+                   :conn cxn
+                   :db (retry/with-retry #(client/db cxn)))))})))
 
 (defmethod f/literal 'vase.datomic.ion/client [_ client-config db-name]
   (->LazyCloudConnection client-config db-name))
